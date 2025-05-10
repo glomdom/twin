0a8be061dbf0c73d556747d29e5b56af39eff445
glomdom <22115@uktc-bg.com>
2025-05-11 02:08:33 +0300

    feat: split logic into base classes

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,60 @@
 import Object from "@rbxts/object-utils";
 import { TweenService } from "@rbxts/services";
 
-export default class TweenBuilder<T extends Instance> {
-  private readonly _instanceRef: T;
+export class TweenBuilderBase<T extends Instance> {
+  protected instance: T;
+  protected tweenTime: number = 1;
+  protected easingStyle: Enum.EasingStyle = Enum.EasingStyle.Linear;
+  protected easingDirection: Enum.EasingDirection = Enum.EasingDirection.Out;
+  protected properties: Partial<ExtractMembers<T, Tweenable>> = {};
+  protected repeatCount: number = 0;
+  protected reverses: boolean = false;
+  protected delayTime: number = 0;
 
-  private _tweenTime: number = 1;
-  private _easingStyle: Enum.EasingStyle = Enum.EasingStyle.Linear;
-  private _easingDirection: Enum.EasingDirection = Enum.EasingDirection.Out;
-  private _properties: Partial<ExtractMembers<T, Tweenable>> = {};
-  private _repeatCount: number = 0;
-  private _reverses: boolean = false;
-  private _delayTime: number = 0;
+  protected onComplete?: () => void;
 
-  private _onCompleteCallback?: () => void;
-  private _onUpdateCallback?: (value: unknown) => void;
-
-  private constructor(instance: T) {
-    this._instanceRef = instance;
-  }
-
-  public static for<T extends Instance>(instance: T): TweenBuilder<T> {
-    return new TweenBuilder(instance);
-  }
-
-  public static forModel(model: Model): TweenBuilder<CFrameValue> {
-    const proxy = new Instance("CFrameValue");
-    proxy.Value = model.GetPivot();
-
-    return new TweenBuilder(proxy).onUpdated((value: CFrame) => model.PivotTo(value));
+  constructor(instance: T) {
+    this.instance = instance;
   }
 
   public time(seconds: number): this {
-    this._tweenTime = seconds;
+    this.tweenTime = seconds;
 
     return this;
   }
 
   public style(style: Enum.EasingStyle): this {
-    this._easingStyle = style;
+    this.easingStyle = style;
 
     return this;
   }
 
   public direction(direction: Enum.EasingDirection): this {
-    this._easingDirection = direction;
+    this.easingDirection = direction;
 
     return this;
   }
 
   public repeat(count: number): this {
-    this._repeatCount = count;
+    this.repeatCount = count;
 
     return this;
   }
 
-  public reverse(should: boolean = true): this {
-    this._reverses = should;
+  public reverse(should = true): this {
+    this.reverses = should;
 
     return this;
   }
 
   public delay(time: number): this {
-    this._delayTime = time;
+    this.delayTime = time;
 
     return this;
   }
 
   public property<K extends ExtractKeys<T, Tweenable>>(prop: K, goal: T[K]): this {
-    this._properties[prop] = goal;
+    this.properties[prop] = goal;
 
     return this;
   }
@@ -75,7 +62,7 @@ export default class TweenBuilder<T extends Instance> {
   public propertiesBulk(props: Partial<ExtractMembers<T, Tweenable>>): this {
     Object.entries(props).forEach(([key, value]) => {
       if (value !== undefined) {
-        this._properties[key as never] = value as never;
+        this.properties[key as never] = value as never;
       }
     });
 
@@ -83,70 +70,80 @@ export default class TweenBuilder<T extends Instance> {
   }
 
   public onCompleted(callback: () => void): this {
-    this._onCompleteCallback = callback;
+    this.onComplete = callback;
 
     return this;
   }
 
-  public onUpdated<V>(callback: (value: V) => void): this {
-    this._onUpdateCallback = callback as (value: unknown) => void;
-
-    return this;
-  }
-
-  public clone(): TweenBuilder<T> {
-    const copy = new TweenBuilder(this._instanceRef);
-    copy._tweenTime = this._tweenTime;
-    copy._easingStyle = this._easingStyle;
-    copy._easingDirection = this._easingDirection;
-    copy._repeatCount = this._repeatCount;
-    copy._reverses = this._reverses;
-    copy._delayTime = this._delayTime;
-    copy._properties = Object.assign({}, this._properties);
-    copy._onCompleteCallback = this._onCompleteCallback;
-    copy._onUpdateCallback = this._onUpdateCallback;
-
-    return copy;
-  }
-
-  public build(): Tween {
-    assert(this._instanceRef !== undefined, "TweenBuilder: No instance provided.");
-
-    if (next(this._properties) === undefined) {
-      warn("TweenBuilder: No properties set for tweening.");
-    }
-
-    const tweenInfo = new TweenInfo(
-      this._tweenTime,
-      this._easingStyle,
-      this._easingDirection,
-      this._repeatCount,
-      this._reverses,
-      this._delayTime,
+  protected buildTween(): Tween {
+    const info = new TweenInfo(
+      this.tweenTime,
+      this.easingStyle,
+      this.easingDirection,
+      this.repeatCount,
+      this.reverses,
+      this.delayTime,
     );
 
-    return TweenService.Create(this._instanceRef, tweenInfo, this._properties);
+    return TweenService.Create(this.instance, info, this.properties);
   }
 
-  /**
-   * Convenience method for building and playing a tween, also runs the provided `onComplete` callback.
-   * @returns The built tween.
-   */
   public play(): Tween {
-    const tween = this.build();
+    const tween = this.buildTween();
 
-    if (this._onCompleteCallback) {
-      tween.Completed.Once(this._onCompleteCallback);
-    }
-
-    if (this._onUpdateCallback && "Value" in this._instanceRef) {
-      const val = this._instanceRef as unknown as ValueBase;
-
-      val.Changed.Connect(this._onUpdateCallback);
+    if (this.onComplete) {
+      tween.Completed.Once(this.onComplete);
     }
 
     tween.Play();
 
     return tween;
+  }
+}
+
+export class TweenValueBuilder<T extends ValueBase> extends TweenBuilderBase<T> {
+  private onUpdate?: (value: T["Value"]) => void;
+
+  constructor(instance: T) {
+    super(instance);
+  }
+
+  public onUpdated(callback: (value: T["Value"]) => void): this {
+    this.onUpdate = callback;
+
+    return this;
+  }
+
+  public override play(): Tween {
+    const tween = this.buildTween();
+
+    if (this.onComplete) {
+      tween.Completed.Once(this.onComplete);
+    }
+
+    if (this.onUpdate) {
+      this.instance.Changed.Connect((newVal) => this.onUpdate!(newVal as T["Value"]));
+    }
+
+    tween.Play();
+
+    return tween;
+  }
+}
+
+export class TweenBuilder {
+  public static for<T extends Instance>(instance: T): TweenBuilderBase<T> {
+    return new TweenBuilderBase(instance);
+  }
+
+  public static forValue<T extends ValueBase>(instance: T): TweenValueBuilder<T> {
+    return new TweenValueBuilder(instance);
+  }
+
+  public static forModel(model: Model) {
+    const proxy = new Instance("CFrameValue");
+    proxy.Value = model.GetPivot();
+
+    return new TweenValueBuilder(proxy).onUpdated((val) => model.PivotTo(val));
   }
 }
